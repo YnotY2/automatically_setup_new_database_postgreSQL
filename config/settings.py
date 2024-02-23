a53f1ead796634d5f378083324f2dfae64d63796
ynoty2 <ynoty2@ynoty2.home>
2024-02-23 16:25:26 +0000

    Initial commit

--- config/settings.py
+++ config/settings.py
@@ -3,17 +3,17 @@ import os
 ## Database PostgreSQL Credentials
 
 # Creating the new user on database-postgreSQL
-postgreSQL_db_new_usr = os.getenv("postgreSQL_db_new_usr", "project_google_reviews_usr")
-postgresql_db_new_passwd = os.getenv("postgresql_db_new_passwd", "Oo51knSSV3OSwfuy8WFu")
+postgreSQL_db_new_usr = os.getenv("postgreSQL_db_new_usr", "<postgreSQL_db_new_usr_here>")        # e.g; "clothing_sales_usr"
+postgresql_db_new_passwd = os.getenv("postgresql_db_new_passwd", "<postgresql_db_new_passwd_here>")        # e.g; "super_secure_password"
 
 # Creating the new database on PostgreSQL
-postgresql_db_new_name = os.getenv("postgresql_db_new_name", "project_google_reviews")
+postgresql_db_new_name = os.getenv("postgresql_db_new_name", "<postgresql_db_new_name_here>")          # e.g; "clothing_sales"
 
 # Credentials used for admin command on PostgreSQL aka "psql" terminal
-existing_postgresql_admin_usr = os.getenv("existing_postgresql_admin_usr", "postgres")
-existing_postgresql_admin_usr_passwd = os.getenv("existing_postgresql_admin_usr_passwd", "postgres")
-existing_postgresql_admin_db = os.getenv("existing_postgresql_admin_db", "postgres")
-existing_postgresql_admin_db_port = os.getenv("existing_postgresql_admin_db_port", "5432")
+existing_postgresql_admin_usr = os.getenv("existing_postgresql_admin_usr", "postgres")                      # e.g; "postgres", default admin-usr.
+existing_postgresql_admin_usr_passwd = os.getenv("existing_postgresql_admin_usr_passwd", "postgres")        # e.g; "postgres", default admin-usr postgres password.
+existing_postgresql_admin_db = os.getenv("existing_postgresql_admin_db", "postgres")                        # e.g; "postgres", default postgres admin password.
+existing_postgresql_admin_db_port = os.getenv("existing_postgresql_admin_db_port", "5432")                  # e.g; "5432", default port.
 
 
 ## Other usefull cmds for manually login db //dont have any effect on code, no variables.
